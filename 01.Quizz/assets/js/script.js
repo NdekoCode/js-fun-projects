8299c142192ab583398f5a86a45eec89c00739aa
NdekoCode <arickbulakali@gmail.com>
2022-07-16 04:41:02 +0200

    Add css part

---
+++ 01.Quizz/assets/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * ALGORITHME
+ * - Recuperer les données au click
+ * - Comparer les resultats.
+ * - Afficher des phrases en fonction des resultats.
+ * - Changer la couleur des blocks + animation en fonction des resulats.
+ */
