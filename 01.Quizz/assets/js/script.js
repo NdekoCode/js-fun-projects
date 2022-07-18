186b1b2e563ed8b871c6bd13f892a0c2b2998714
NdekoCode <arickbulakali@gmail.com>
2022-07-18 05:03:50 +0200

    feat: get formQuestion  response

--- 01.Quizz/assets/js/script.js
+++ 01.Quizz/assets/js/script.js
@@ -5,3 +5,15 @@
  * - Afficher des phrases en fonction des resultats.
  * - Changer la couleur des blocks + animation en fonction des resulats.
  */
+const form = document.querySelector(".form-quizz");
+const questionsBlock = form.querySelectorAll(".question-block");
+let tableauResult = [];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  for (let i = 1; i < questionsBlock.length + 1; i++) {
+    const formResponse = form.querySelector(`input[name="q${i}"]:checked`);
+    tableauResult.push(formResponse.value);
+  }
+  console.log(tableauResult);
+  tableauResult.length = 0;
+});
